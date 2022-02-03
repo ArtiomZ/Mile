751842198565eaa5714ec8713c14c9ee1d33cdc0
Артём Журавлёв <artemon50309@gmail.com>
2022-02-03 14:48:09 +0200

    initial commit

---
+++ src/Mile.java
@@ -0,0 +1,15 @@
+import java.net.SocketOption;
+
+public class Mile {
+
+    public static void main(String[] args) {
+        int ticket = 1500;
+        int mileСost = 20;
+
+        int bonusMile = ticket / mileСost;
+
+        System.out.println("Количество бонусных милль: " + bonusMile);
+
+
+    }
+}
